dba04b7d5a090a6861d7dae0669e898869db352d
LittleFlyers <2538285284@qq.com>
2017-07-26 11:43:30 +0800

    123

--- knowledge/controller/Library.php
+++ knowledge/controller/Library.php
@@ -5,7 +5,12 @@ class Library{
 	//获取文库中全部的信息
 	public function all()
 	{
+		$L = db('document');
+		$list = $L->select();
+		$result['err_code'] = 0;
+		$result['data'] = $list;
 		
+		return json_encode($result);
 	}
 	//获取文库中的单个文件
 	public function one()
